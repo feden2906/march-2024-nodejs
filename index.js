adb5df159ddd8940bd8f5a51d65b44f5f3790b99
feden2906 <feden2906@gmail.com>
2024-09-05 19:18:10 +0300

    hw-2

--- index.js
+++ index.js
@@ -1,105 +1,63 @@
-const http = require('node:http');
 const path = require('node:path');
-const fs = require('node:fs');
-const fsPromises = require('node:fs/promises');
-const readline = require('node:readline/promises');
-const EventEmitter = require('node:events');
-const os = require('os');
+const fs = require('node:fs/promises');
 
-
-const {foo: helperFoo} = require('./helpers/helper');
 
 const foo = async () => {
-    //Http
-    const server = http.createServer((req, res) => {
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify({
-            data: 'Hello World!',
+    try {
+        const pathToDir = path.join(__dirname, 'baseFolder');
+        await fs.mkdir(pathToDir, {recursive: true});
+
+        const folderNames = ['folder1', 'folder2', 'folder3', 'folder4', 'folder5'];
+        const fileNames = ['file1.txt', 'file2.txt', 'file3.txt', 'file4.txt', 'file5.txt'];
+
+        await Promise.all(folderNames.map(async (folder) => {
+            const folderPath = path.join(pathToDir, folder);
+            await fs.mkdir(folderPath, {recursive: true});
+
+            await Promise.all(fileNames.map(async (file) => {
+                const filePath = path.join(folderPath, file);
+                await fs.writeFile(filePath, 'Hello World!');
+            }));
         }));
-    });
-    server.listen(3000);
 
-    // Path
-    const pathToFile = __filename;
-    console.log(pathToFile)
-    console.log(path.dirname(pathToFile))
-    console.log(path.extname(pathToFile))
-    console.log(path.basename(pathToFile))
-    console.log(path.parse(pathToFile))
-    console.log(path.isAbsolute(pathToFile))
-    console.log(path.isAbsolute('./Lessons/march-2024'))
+        const data = await fs.readdir(pathToDir);
+        for (const folder of data) {
+            const folderPath = path.join(pathToDir, folder);
+            const stat = await fs.stat(folderPath);
+            console.log(`Folder: ${folder}, isDirectory: ${stat.isDirectory()}`);
 
-    // Readline
-    const rlInstance = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    })
-    const name = await rlInstance.question('Name?');
-    console.log(`Your name is ${name}`)
-    process.exit(0)
-
-    // FS
-    const pathToFile = path.join(__dirname, 'test.txt');
-    await fsPromises.writeFile(pathToFile, 'Hello World!\n')
-    const data = await fsPromises.readFile(pathToFile, 'utf-8')
-    console.log(data);
-    await fsPromises.appendFile(pathToFile, 'Some new data')
-    await fsPromises.mkdir(path.join(__dirname, 'new-folder'), {recursive: true})
-    // await fsPromises.mkdir(path.join(__dirname, 'new-folder', 'another-folder', 'another-folder'))
-    // await fsPromises.mkdir(path.join(__dirname, 'new-folder', 'another-folder', 'another-folder'), {recursive: true})
-    // await fsPromises.rm(path.join(__dirname, 'new-folder'), {recursive: true})
-
-    // await fsPromises.unlink(pathToFile) // delete file
-    // await fsPromises.rename(pathToFile, path.join(__dirname, 'new-folder', 'new-file.txt'))
-    await fsPromises.copyFile(pathToFile, path.join(__dirname, 'new-folder', 'new-file.txt'))
-    const stat = await fsPromises.stat(pathToFile)
-    console.log(stat.isFile())
-
-    // Streams
-    const pathToFile = path.join(__dirname, '25900.pdf');
-    const readStream = fs.createReadStream(pathToFile);
-    const writeStream = fs.createWriteStream(path.join(__dirname, 'new-big-file.pdf'));
-    // readStream.on('data', (chunk) => {
-    //     console.log('chunk', chunk.length)
-    //     writeStream.write(chunk)
-    // })
-    readStream.pipe(writeStream);
-
-    // Events
-    const emitter = new EventEmitter();
-    emitter.on('event2', () => {
-        console.log('Event happened')
-    })
-    emitter.emit('event2')
-    emitter.emit('event2')
-    emitter.emit('event2')
-    emitter.emit('event2')
-    emitter.emit('event2')
-    emitter.emit('event2')
-    emitter.once('event2', (...args) => {
-        console.log('________________');
-        console.log('Event 1 happened');
-        console.log(args)
-        console.log('________________');
-    })
-    emitter.on('event2', (...args) => {
-        console.log('Event 2 happened')
-        console.log(args)
-    })
-    emitter.emit('event2', 'Hello', 345, 44)
-    emitter.emit('event2')
-    emitter.emit('event2', 555)
-
-    // OS
-    console.log(os.arch());
-    console.log(os.cpus());
-    console.log(os.totalmem() / 1024 / 1024 / 1024, 'gb');
-    console.log(os.freemem() / 1024 / 1024 / 1024, 'gb');
-    console.log(os.homedir());
-    console.log(os.hostname());
-    console.log(os.platform());
-    console.log(os.userInfo());
+            const files = await fs.readdir(folderPath);
+            for (const file of files) {
+                const pathToFile = path.join(folderPath, file);
+                const fileStat = await fs.stat(pathToFile);
+                console.log(`File: ${file}, isFile: ${fileStat.isFile()}`);
+            }
+        }
+    } catch (e) {
+        console.error(e.message);
+    }
 }
 
 
 void foo();
+
+
+// const fs = require('node:fs/promises');
+// const path = require("node:path");
+//
+// const homeWork = async () => {
+//     const baseFolder = path.join(__dirname, 'baseFolder')
+//     await fs.mkdir(baseFolder, {recursive: true});
+//
+//     for (let i = 1; i < 6; i++) {
+//         const currentFolder = path.join(baseFolder, `folder-${i}`)
+//         await fs.mkdir(currentFolder, {recursive: true});
+//
+//         for (let j = 1; j < 6; j++) {
+//             await fs.writeFile(path.join(currentFolder, `baseFile-${j}`), 'aaaaaaaaaaaaaaaa')
+//         }
+//     }
+//
+// }
+//
+// void homeWork()
